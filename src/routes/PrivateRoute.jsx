d703a057a2827275efc9b64af36f77476686e686
Abdur Rahman Mahmud <ar7543078@gmail.com>
2025-02-21 14:06:30 +0600

    add PrivateRoute component for protected route handling

---
+++ src/routes/PrivateRoute.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable react/prop-types */
+import { Navigate, useLocation } from "react-router-dom";
+
+
+import useAuth from "../hooks/useAuth";
+
+
+const PrivateRoute = ({ children }) => {
+  const { user, loading } = useAuth()
+
+  const location = useLocation();
+
+//   if (loading) return <LoadingSpinner />;
+  if (user) return children;
+  return <Navigate to="/login" state={location.pathname} />;
+};
+
+export default PrivateRoute;
